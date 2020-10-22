4a153f6d7a11b9152843d81b8172d304117d4a32
Анастасия Птах <nastya@MacBook-Pro-Anastasia.local>
2020-10-22 11:27:29 +0300

    adaptive layout

--- app/js/main.js
+++ app/js/main.js
@@ -25,8 +25,14 @@ $(function(){
     });
     
     $('.menu__btn').on('click' , function() {
-        $('.menu__btn').toggleClass('.menu__btn--active');
+        $('.menu__btn').toggleClass('menu__btn--active');
+        $('.menu__list').toggleClass('menu__list--active');
     });
 
+
+    $('.menu__list-link').on('click' , function() {
+        $('.menu__btn').removeClass('menu__btn--active');
+        $('.menu__list').removeClass('menu__list--active');
+    });
   
 });
